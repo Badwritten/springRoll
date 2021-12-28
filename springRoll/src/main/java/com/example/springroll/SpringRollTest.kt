90a7e464d1ef8eaae23cad9d67f30e91094af118
wanghs <wanghaishu@dongfangfuli.com>
2021-12-28 10:10:57 +0800

    fourth commit

---
+++ springRoll/src/main/java/com/example/springroll/SpringRollTest.kt
@@ -0,0 +1,13 @@
+package com.example.springroll
+
+/**
+ * create by wanghs
+ * create on 2021/12/28 10:01
+ * description  test gradle implement
+ */
+class SpringRollTest {
+    fun sumSpringRoll(num: Int): Int {
+        return num * 10
+    }
+
+}
